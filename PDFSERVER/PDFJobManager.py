b739de206323db10bae75cb89f9bd07bc8dd2cd0
Austin Brown <austinbrown34@gmail.com>
2016-08-21 23:49:25 -0700

    removes returns from __init__

--- PDFSERVER/PDFJobManager.py
+++ PDFSERVER/PDFJobManager.py
@@ -3,6 +3,14 @@ import imp
 
 class PDFJobManager(object):
     def __init__(self, job_type, server_data):
+        self.job_type = job_type
+        self.server_data = server_data
+
+    def load_job(self, job_type=None, server_data=None):
+        if job_type is None:
+            job_type = self.job_type
+        if server_data is None:
+            server_data is self.server_data
         try:
             job = imp.load_source(
                 job_type,
