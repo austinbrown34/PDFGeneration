c58a8f63e951fc77b00e2518e8e705a32b228bf3
Austin Brown <austinbrown34@gmail.com>
2016-08-23 11:39:03 -0700

    adding a bootstrap script to get everything set up

---
+++ PDFSERVER/bootstrap.sh
@@ -0,0 +1,85 @@
+# ensure yum is up to date
+
+sudo yum update
+
+# let's get python 2.7 installed with pip and source it
+
+sudo yum install python27
+sudo easy_install pip
+echo "alias python='python27'" >> ~/.bashrc
+source ~/.bashrc
+
+# install some python development helpers
+
+sudo yum install -y gcc-c++
+sudo yum install -y libxml2-python libxml2-devel
+sudo yum install -y python-devel
+
+# now we must install uwsgi, Flask, and nginx to be a real server
+
+sudo easy_install uwsgi
+sudo easy_install Flask
+sudo yum install -y nginx
+
+# the following installs will help us work with images
+
+sudo yum install libjpeg-devel
+sudo pip install pillow
+sudo pip install piexif
+
+# finally we get to the pdf specific libraries
+
+sudo pip install pdfminer
+sudo pip install fdfgen
+sudo pip install reportlab
+sudo pip install pypdf2
+
+# we must install some helper dependencies that assist with fonts
+
+sudo yum install fontconfig freetype freetype-devel fontconfig-devel libstdc++
+sudo yum install fontconfig freetype freetype-devel fontconfig-devel libstdc++
+
+# in order to have flexibility over data visualizations, we need phantomjs
+
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo mkdir -p /opt/phantomjs
+bzip2 -d phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo tar -xvf phantomjs-2.1.1-linux-x86_64.tar --directory /opt/phantomjs/ --strip-components 1
+
+# let's get git
+
+sudo yum install git
+
+# to take care of some xml parsing needs we'll need these as well
+
+sudo yum install libxslt-devel
+sudo pip install lxml
+
+# to work with S3 we need to grab boto
+
+sudo pip install boto3
+
+# PDFtk - a free java application - helps us accomplish a couple important tasks
+# unfortunately, PDFtk doesn't compile natively on Amazon Linux - so we will
+# grab a precompiled version of it
+
+git clone https://github.com/lob/lambda-pdftk-example.git
+sudo mkdir -p /opt/pdftk
+sudo cp lambda-pdftk-example/bin/pdftk /opt/pdftk/
+sudo cp lambda-pdftk-example/bin/libgcj.so.10 /opt/pdftk/
+sudo ln -s /opt/pdftk/pdftk /usr/bin/pdftk
+sudo ln -s /opt/pdftk/libgcj.so.10 /usr/lib/libgcj.so.10
+sudo cp /opt/pdftk/libgcj.so.10 /usr/lib64/
+export LD_LIBRARY_PATH=/opt/pdftk/libgcj.so.10
+
+# Finally, let's replace the nginx config file with our own and fire her up
+
+sudo cp -f nginx.conf /etc/nginx/nginx.conf
+sudo service nginx start
+sudo chkconfig nginx on
+sudo uwsgi --yaml app.yaml
+
+# Though, the docs specify using (kill -INT `cat /tmp/uwsgi.pid`)
+# to stop all uwsgi processes, if this doesn't work, this should:
+# (sudo kill `pidof uwsgi`) - just be sure to restart when you need it again
+# Also, to check your logs do this - sudo cat /var/log/uwsgi.log
