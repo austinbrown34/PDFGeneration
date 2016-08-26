4b54d303b137997044c0652bb6bc841ff5cc65f3
Austin Brown <austinbrown34@gmail.com>
2016-08-26 13:06:11 -0700

    added get_tag command to placeholders cltool

--- PDFSERVER/cltools/placeholders/placeholders/setup.py
+++ PDFSERVER/cltools/placeholders/placeholders/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='placeholders',
-    version='0.8',
+    version='1.1',
     packages=find_packages(),
     description='Create placeholder images by embedding keywords into regular jpg images.',
     author='Austin Brown',
     author_email='austinbrown34@gmail.com',
     url='https://github.com/austinbrown34/placeholders',
-    download_url='https://github.com/austinbrown34/placeholders/tarball/0.8',
+    download_url='https://github.com/austinbrown34/placeholders/tarball/1.1',
     keywords=['image', 'exif', 'jpg'],
     py_modules=['placeholders', 'placeholder_tools'],
     classifiers=[],
@@ -20,6 +20,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        placeholders=placeholders:make_placeholders
+        placeholders=placeholders:cli
     ''',
 )
