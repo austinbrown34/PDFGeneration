2c05cbe8be62de56278828cc11940702475f5e49
EC2 Default User <ec2-user@ip-172-31-0-249.us-west-1.compute.internal>
2016-08-27 04:06:11 +0000

    wip

--- PDFSERVER/lambda_deployment/PDFServerAPI.py
+++ PDFSERVER/lambda_deployment/PDFServerAPI.py
@@ -3,8 +3,19 @@ from PDFManager import PDFManager
 
 app = Flask(__name__)
 import sys
+import json
+import os
 
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+def unhandled_exceptions(e, event, context):
+    print "e:"
+    print e
+    print "event:"
+    print event
+    print "context:"
+    print context
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
@@ -12,14 +23,18 @@ def not_found(error):
 
 @app.route('/v1/generate', methods=['POST'])
 def generate_reports():
-    print "hello"
+    print request
     if not (request.json):
         abort(400)
-    
-    with open('log.txt', 'w') as f:
-        sys.stdout = f
-        pdf = PDFManager(request.json)
-        response = pdf.get_job()
+    print "this is request.json"
+    if not os.path.exists('/tmp'):
+        os.makedirs('/tmp')
+    with open('/tmp/fullpost.txt', 'w') as p:
+        p.write(str(request.json))
+    print "wrote to some files"
+    pdf = PDFManager(request.json)
+    print "pdfmanager initialized"
+    response = pdf.get_job()
 
     return jsonify(response)
 
