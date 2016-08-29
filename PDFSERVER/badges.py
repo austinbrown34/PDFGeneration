1468ffb3219d874375de3ebff18236708bf205c4
Austin Brown <austinbrown34@gmail.com>
2016-08-28 23:16:42 -0700

    stripped down to include bare essentials for lambda server

--- PDFSERVER/badges.py
+++
@@ -1,7 +0,0 @@
-def run(data):
-    new_data = data
-    new_data['badges'] = {
-        'good': True,
-        'bad': False
-        }
-    return new_data
--- PDFSERVER/js/sparkline.js
+++
@@ -1,8 +0,0 @@
-var reportdata =
-[['cbc', 4.0, 65.0], ['cbda', 7.0, 65.0], ['cbn', 7.0, 65.0], ['cbg', 5.0, 65.0], ['cbd', 22.0, 65.0], ['thca', 3.0, 65.0], ['d8_thc', 0.0, 65.0], ['d9_thc', 13.0, 65.0], ['thcv', 4.0, 65.0]];
-var reportdimensions =
-[267, 248];
-var reportcoords =
-[24, 118];
-
-var collection = [reportdata, reportdimensions, reportcoords]
