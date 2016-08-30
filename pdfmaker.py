23a75537c7c7fc600e51cbc3a6c57776b79623ba
Austin Brown <austinbrown34@gmail.com>
2016-08-30 14:04:49 -0700

    fixed path to pdftk library and pdftk

--- pdfmaker.py
+++ pdfmaker.py
@@ -9,6 +9,7 @@ class PDFMaker(object):
         self.templates = templates
         self.server_data = server_data
         self.total_pages = len(templates)
+        os.environ['LD_LIBRARY_PATH'] = os.path.joing(os.getcwd(), 'bin', 'libgcj.so.10')
 
     def make_pages(
             self,
