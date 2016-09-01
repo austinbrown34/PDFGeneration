a18386d555fe1953c7827387baa3a641a6ee880c
Austin Brown <austinbrown34@gmail.com>
2016-09-01 16:16:10 -0700

    removed unnecessary top-level assets

--- js/sparkline.js
+++
@@ -1,8 +0,0 @@
-var reportdata =
-[['cbc', 4.0, 69.0], ['cbda', 7.0, 69.0], ['cbn', 7.0, 69.0], ['cbg', 5.0, 69.0], ['cbd', 22.0, 69.0], ['thca', 3.0, 69.0], ['d8_thc', 4.0, 69.0], ['d9_thc', 13.0, 69.0], ['thcv', 4.0, 69.0]];
-var reportdimensions =
-[267, 248];
-var reportcoords =
-[24, 118];
-
-var collection = [reportdata, reportdimensions, reportcoords]
