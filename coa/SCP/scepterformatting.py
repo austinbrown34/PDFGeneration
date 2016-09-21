5bb903dfdfd4205af60d78fe5a6635cbc62c4e06
Austin Brown <austinbrown34@gmail.com>
2016-09-20 18:50:40 -0700

    fixed moisture in scp script

--- coa/SCP/scepterformatting.py
+++ coa/SCP/scepterformatting.py
@@ -1,8 +1,29 @@
 import os
+from decimal import Decimal
+def make_number(data, digits=None, labels=False):
+    try:
+        data = float(data)
+    except ValueError:
+        if not labels:
+            data = float(0)
+        if data == "<LOQ":
+            data = "&ltLOQ"
+
+    output = data
+    if digits is not None and isinstance(data, float):
+        dec = Decimal(data)
+        output = round(dec,int(digits))
+    return output
+
 def run(data, templates, s3templates):
     new_data = data
     new_templates = templates
-
+    try:
+        special_moisture = str(data['special']['moisture'])
+        special_moisture = make_number(special_moisture.replace('%', ''), 2, True)
+        if str(special_moisture) != '' and str(special_moisture) not in ['ND', 'NR']:
+                special_moisture = str(special_moisture) + '%'
+        data['special']['moisture'] = special_moisture
     # try:
     #     cannabinoid_datatable_data = data['viz']['datatable_cannabinoids']
     #     new_cannabinoid_datatable_data = []
@@ -10,9 +31,9 @@ def run(data, templates, s3templates):
     #         new_analyte_data = [analyte_data[0], analyte_data[2], analyte_data[3]]
     #         new_cannabinoid_datatable_data.append(new_analyte_data)
     #     new_data['viz']['datatable_cannabinoids-scp'] = new_cannabinoid_datatable_data
-    # except Exception as e:
-    #     print str(e)
-    #     print "made it to the scepterformatting exception"
-    #     pass
+    except Exception as e:
+        print str(e)
+        print "made it to the scepterformatting exception"
+        pass
     response = {'data': new_data, 'templates': new_templates}
     return response
