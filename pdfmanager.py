6331f587a1b47cb99fae917dc6dc238f1a3d5cc2
Vagrant Default User <vagrant@jessie.raw>
2016-10-09 10:01:33 +0000

    bcolors add and disable post

--- pdfmanager.py
+++ pdfmanager.py
@@ -6,6 +6,16 @@ import json
 import subprocess
 import os
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class PDFManager(object):
 
     def __init__(self, payload):
